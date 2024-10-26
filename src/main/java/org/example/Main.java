77208603cd296e8dec6b6f140f6da2f47c7d0336
Nikolaj Ignatieff Schwartzbach <nschwartzbach@hotmail.com>
2024-10-26 18:33:56 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import javax.swing.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // Create a new JFrame
+        JFrame frame = new JFrame("Deepest Strike");
+
+        // Set frame properties
+        frame.setSize(800,
+                600); // Set the size of the frame
+
+        // Close operation
+        frame.setDefaultCloseOperation(
+                JFrame.EXIT_ON_CLOSE);
+
+        // Make the frame visible
+        frame.setVisible(true);
+
+    }
+}
