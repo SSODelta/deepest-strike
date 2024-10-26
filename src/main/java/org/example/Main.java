a4e14620c8f45bdc40af1b67be65a7fffeb7e6bc
Nikolaj Ignatieff Schwartzbach <nschwartzbach@hotmail.com>
2024-10-26 18:37:31 +0200

    second commit lol

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -9,6 +10,13 @@ public class Main {
 
         // Create a new JFrame
         JFrame frame = new JFrame("Deepest Strike");
+        JPanel panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, null);
+            }
+        }
 
         // Set frame properties
         frame.setSize(800,
